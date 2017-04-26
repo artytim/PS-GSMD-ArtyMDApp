a36d836aeaef6f1ef479cf700dc6c0ff9a3505f3
artytim <arty.tim@gmail.com>
2017-04-26 17:47:03 +0800

    Toolbar: Using Toolbar as ActionBar

--- app/src/main/java/com/artytect/android/artysmdapp/MainActivity.java
+++ app/src/main/java/com/artytect/android/artysmdapp/MainActivity.java
@@ -1,21 +1,32 @@
 package com.artytect.android.artysmdapp;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Check if we're running on Android 5.0 or higher
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            // Call some material design APIs here
-        } else { // For Below API 21
-            // Implement this feature without material design
-        }
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("Welcome !");
+        toolbar.setSubtitle("Folks !");
+
+        toolbar.setLogo(R.drawable.good_day);
+        toolbar.setNavigationIcon(R.drawable.navigation_back);
+
+//        // Check if we're running on Android 5.0 or higher
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            // Call some material design APIs here
+//        } else { // For Below API 21
+//            // Implement this feature without material design
+//        }
     }
 }
